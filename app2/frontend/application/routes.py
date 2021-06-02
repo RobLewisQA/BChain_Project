32419cb8e2d6088027ed13162b9e9dd588359658
RobLewisQA <rlewis@academytrainee.com>
2021-06-02 10:54:47 +0000

    working keypair generator anc frontend display

--- app2/frontend/application/routes.py
+++ app2/frontend/application/routes.py
@@ -5,24 +5,47 @@ from flask import Flask, redirect, request, url_for,render_template, Response, j
 from application import app
 import requests
 
+@app.route('/') 
+@app.route('/home') 
+def home():
+    return render_template('homepage.html')
+
 @app.route('/login', methods=['GET','POST']) 
 def login():
     return render_template('login.html')
 
-@app.route('/gateway', methods=['GET','POST']) 
-def gateway():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        reg_list = requests.get('http://Database:5006/database_router').text
-        df = pd.read_json(reg_list)
-        if len(df.loc[df.username == username]) > 0:
-            if df.loc[df.username == username].password.max() == password:
-                return df.loc[df.username == username].to_html()
-            else:
-                return "Oops, that password wasn't correct" 
-        else:
-            return "Oops, there's no user with that username"
+@app.route('/signup', methods=['GET','POST']) 
+def signup():
+    keypair = requests.get('http://keypair-generator:5001/keys_generator').text
+    return pd.read_json(keypair).to_html()
+    #return render_template('signup.html')
+
+@app.route('/account', methods=['GET']) 
+def account():
+    return render_template('account.html')
+
+@app.route('/mine', methods=['GET','POST']) 
+def mine():
+    return render_template('mine.html')
+
+@app.route('/transact', methods=['GET','POST']) 
+def transact():
+    return render_template('transact.html')
+
+# @app.route('/gateway', methods=['GET','POST']) 
+# def gateway():
+#     if request.method == 'POST':
+#         username = request.form['username']
+#         password = request.form['password']
+#         reg_list = requests.get('http://Database:5006/database_router').text
+#         df = pd.read_json(reg_list)
+#         if len(df.loc[df.username == username]) > 0:
+#             if df.loc[df.username == username].password.max() == password:
+#                 return df.loc[df.username == username].to_html()
+#             else:
+#                 return "Oops, that password wasn't correct" 
+#         else:
+#             return "Oops, there's no user with that username"
     
 
 
--- app2/keypair-generator/application/routes.py
+++ app2/keypair-generator/application/routes.py
@@ -1,20 +1,23 @@
 import random
-import application.BChain_WordList as bwords
+import application.words as words
 import pandas as pd
 from flask import Flask, redirect, request, url_for,render_template, Response, jsonify
 from application import app
+import hashlib
 
 @app.route('/keys_generator', methods=['GET']) 
 def keys_generator():
     wordlib = words.wordlib
     wordstr = ""
+    wordlist = []
 
     while len(wordlist) < 12:
         word = wordlib[random.randint(0,len(wordlib)-1)]
         wordstr = wordstr + word + "_"
+        wordlist = wordlist + [word]
         wordlib.remove(word)
 
     mnemonic = wordstr
     private_key = hashlib.sha256(mnemonic.encode()).hexdigest()
     public_key = hashlib.sha256(private_key.encode()).hexdigest()
-    return pd.DataFrame([private_key,public_key],index=['private_key','public_key']).to_json()
+    return pd.DataFrame([private_key,public_key,mnemonic],index=['private_key','public_key','mnemonic']).to_json()
