60f1c74c9ef45941dbdbd4163e559a6b7afc623f
RobLewisQA <rlewis@academytrainee.com>
2021-12-20 19:23:22 +0000

    kubernetes changes working; docker-compose presenting networking issues

--- scripts/deployapp.sh
+++ scripts/deployapp.sh
@@ -1,14 +1,11 @@
 cd kubernetes
-#eksctl create cluster --name bchain-cluster --region eu-west-1 --nodegroup-name bchain-node-group --nodes 4 --nodes-min 4 --nodes-max 5 --node-type t2.micro --with-oidc --ssh-access --ssh-public-key aws-myfirstkeypair --managed
-aws eks --region eu-west-1 update-kubeconfig --name eks_cluster_bchain_project
-kubectl apply -f wordlist_reader.yaml
-kubectl apply -f wordlist_parser.yaml
-kubectl apply -f username_generator.yaml
-kubectl apply -f password_generator.yaml
-kubectl apply -f seedphrase_generator.yaml
-kubectl apply -f pandas_jsonreader.yaml
-kubectl apply -f database.yaml
-kubectl apply -f frontend_login.yaml
+eksctl create cluster --name bchain-cluster --region eu-west-2 --nodegroup-name bchain-node-group --nodes 3 --nodes-min 3 --nodes-max 3 --node-type t2.micro --with-oidc --ssh-access --ssh-public-key aws_thirdkeypair --managed
+# eksctl create cluster --name DemoCluster --region eu-west-2 --nodegroup-name DemoNodes --nodes 4 --nodes-min 4 --nodes-max 5 --node-type t2.micro --with-oidc --ssh-access --ssh-public-key aws_thirdkeypair --managed
+#aws eks --region eu-west-2 update-kubeconfig --name eks_cluster_bchain_project
+kubectl apply -f frontend.yaml
+kubectl apply -f blockchain-engine.yaml
+kubectl apply -f credentials.yaml
+kubectl apply -f keypair-generator.yaml
 kubectl apply -f ./nginx/service.yaml
 kubectl apply -f ./nginx/deployment.yaml
 kubectl apply -f ./nginx/configmap.yaml
